b9c6e46c1cb9184f3c3ea5225d82540bcaf475ab
Johnny Toumieh <johnnytoumieh@live.com>
2018-04-09 17:11:01 +0300

    now patients go out correctly of scope + still not dynamic

--- Patient.cpp
+++ Patient.cpp
@@ -97,7 +97,7 @@ void Patient::checkGameState() {
  */
 
 void Patient::update(){
-    if (x() + 30 > 1000) {
+    if (x() + 30 > 1100) {
         motionState == Left;
         //toDelete = true;
         speedTimer->stop();
